fc0ace9f2a1c40684af6b1bd0fabcaa0c8617d4f
Bert Münnich <ber.t@posteo.de>
2017-10-05 13:49:11 +0200

    Simplify cursor reset in image mode
    No need to give cursor name. reset_cursor() is now the only place to decide
    what cursor to fall back to.

--- main.c
+++ main.c
@@ -611,8 +611,8 @@ void on_buttonpress(XButtonEvent *bev)
 	static Time firstclick;
 
 	if (mode == MODE_IMAGE) {
-		win_set_cursor(&win, CURSOR_ARROW);
 		set_timeout(reset_cursor, TO_CURSOR_HIDE, true);
+		reset_cursor();
 
 		for (i = 0; i < ARRLEN(buttons); i++) {
 			if (buttons[i].button == bev->button &&
@@ -772,8 +772,8 @@ void run(void)
 				break;
 			case MotionNotify:
 				if (mode == MODE_IMAGE) {
-					win_set_cursor(&win, CURSOR_ARROW);
 					set_timeout(reset_cursor, TO_CURSOR_HIDE, true);
+					reset_cursor();
 				}
 				break;
 		}
